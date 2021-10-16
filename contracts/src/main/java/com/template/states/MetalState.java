7dd31ebfec00e1a10b77e6af3861382b45941ba0
Fang Yuan <fangyuan@Fangs-MacBook-Pro.local>
2021-10-16 13:43:15 +0800

    reward

---
+++ contracts/src/main/java/com/template/states/MetalState.java
@@ -0,0 +1,47 @@
+package com.template.states;
+
+import com.template.contracts.MetalContract;
+import com.template.contracts.TemplateContract;
+import net.corda.core.contracts.BelongsToContract;
+import net.corda.core.contracts.ContractState;
+import net.corda.core.identity.AbstractParty;
+import net.corda.core.identity.Party;
+
+import java.util.Arrays;
+import java.util.List;
+
+// *********
+// * State *
+// *********
+@BelongsToContract(MetalContract.class)
+public class MetalState implements ContractState {
+
+    private String customer;
+    private int point;
+    private Party issuer;
+    private Party owner;
+
+    public MetalState(String customer, int point, Party issuer, Party owner) {
+        this.customer = customer;
+        this.point = point;
+        this.issuer = issuer;
+        this.owner = owner;
+    }
+
+    @Override
+    public List<AbstractParty> getParticipants() {
+        return Arrays.asList(issuer, owner);
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public Party getIssuer() {return issuer;}
+    public Party getOwner() {return owner;}
+
+}
---
+++ workflows/src/main/java/com/template/flows/TransferMetal.java
@@ -0,0 +1,153 @@
+package com.template.flows;
+
+import co.paralleluniverse.fibers.Suspendable;
+import com.template.contracts.MetalContract;
+import com.template.states.MetalState;
+import net.corda.core.contracts.Command;
+import net.corda.core.contracts.StateAndRef;
+import net.corda.core.flows.*;
+import net.corda.core.identity.Party;
+import net.corda.core.node.services.Vault;
+import net.corda.core.node.services.vault.QueryCriteria;
+import net.corda.core.transactions.SignedTransaction;
+import net.corda.core.transactions.TransactionBuilder;
+import net.corda.core.utilities.ProgressTracker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// ******************
+// * Initiator flow *
+// ******************
+@InitiatingFlow
+@StartableByRPC
+public class TransferMetal extends FlowLogic<SignedTransaction> {
+
+    private String customer;
+    private int point;
+    private Party newOwner;
+    private int input = 0;
+
+    public TransferMetal(String customer, int point, Party newOwner) {
+        this.customer = customer;
+        this.point = point;
+        this.newOwner = newOwner;
+    }
+
+    private final ProgressTracker.Step RETRIEVING_NOTARY = new ProgressTracker.Step("Retrieving the Notary.");
+    private final ProgressTracker.Step GENERATING_TRANSACTION = new ProgressTracker.Step("Generating transaction.");
+    private final ProgressTracker.Step SIGNING_TRANSACTION = new ProgressTracker.Step("Signing transaction with our private key.");
+    private final ProgressTracker.Step COUNTERPARTY_SESSION = new ProgressTracker.Step("Sending flow to counterparty.");
+    private final ProgressTracker.Step FINALISING_TRANSACTION = new ProgressTracker.Step("Obtaining notary signature and recording transaction");
+
+    private final ProgressTracker progressTracker = new ProgressTracker(
+            RETRIEVING_NOTARY,
+            GENERATING_TRANSACTION,
+            SIGNING_TRANSACTION,
+            COUNTERPARTY_SESSION,
+            FINALISING_TRANSACTION
+    );
+
+    @Override
+    public ProgressTracker getProgressTracker() {
+        return progressTracker;
+    }
+
+    //    ----------------------------------------------- Check for Metal States Starts-----------------------------------------------
+
+    List<StateAndRef<MetalState>> checkForMetalStates() throws FlowException {
+
+        //Ok for gradle compilation, why IntelliJ complain?
+        QueryCriteria generalCriteria = new QueryCriteria.VaultQueryCriteria(Vault.StateStatus.UNCONSUMED);
+
+
+        List<StateAndRef<MetalState>> MetalStates = getServiceHub().getVaultService().queryBy(MetalState.class, generalCriteria).getStates();
+        List<StateAndRef<MetalState>> result = new ArrayList<>();
+
+        boolean inputFound = false;
+        int size = MetalStates.size();
+        int totalPoint = 0;
+        for (int i = 0; i < size; i++) {
+            if (MetalStates.get(i).getState().getData().getCustomer().equals(customer)
+            && totalPoint < point) { //burn the point for input
+                result.add(MetalStates.get(i));
+                totalPoint += MetalStates.get(i).getState().getData().getPoint();
+            }
+        }
+
+
+        if (result.size()==0) {
+            System.out.println("\n Input not found");
+            throw new FlowException();
+        }
+
+        return result;
+    }
+
+    int findUnBurnedPoint(List<StateAndRef<MetalState>> input, int pointToRedeem) {
+        int totalInputPoint = 0;
+        for (int i=0; i<input.size(); i++) {
+            totalInputPoint += input.get(i).getState().getData().getPoint();
+        }
+        return (totalInputPoint - pointToRedeem);
+    }
+
+    //    ----------------------------------------------- Check for Metal States Ends-----------------------------------------------
+
+    @Suspendable
+    @Override
+    public SignedTransaction call() throws FlowException {
+        // Initiator flow logic goes here.
+
+        // Retrieve Notary Identity
+        progressTracker.setCurrentStep(RETRIEVING_NOTARY);
+        Party notary = getServiceHub().getNetworkMapCache().getNotaryIdentities().get(0);
+
+        List<StateAndRef<MetalState>> inputStates = null;
+
+        inputStates = checkForMetalStates();
+
+        Party issuer = inputStates.get(0).getState().getData().getIssuer();
+
+
+        //Output State will be the redemption + unburned points for last reward
+         int unburnedPoint = findUnBurnedPoint(inputStates, point);
+         MetalState outputState = null;
+         if (unburnedPoint>0) {
+             outputState = new MetalState(customer, unburnedPoint, issuer, getOurIdentity()); //unburned points
+         }
+
+        //Create transaction components
+        //MetalState outputState = new MetalState(metalName, weight, issuer, newOwner);
+        Command cmd = new Command(new MetalContract.Transfer(), getOurIdentity().getOwningKey());
+
+
+        // Create transaction builder
+        progressTracker.setCurrentStep(GENERATING_TRANSACTION);
+        TransactionBuilder txB = new TransactionBuilder(notary);
+        if (outputState!=null) txB.addOutputState(outputState, MetalContract.CID);
+        txB.addCommand(cmd);
+
+        for (int i=0; i<inputStates.size(); i++) {
+            txB.addInputState(inputStates.get(i));
+        }
+
+
+        // Sign the transaction
+        progressTracker.setCurrentStep(SIGNING_TRANSACTION);
+        SignedTransaction signedTx = getServiceHub().signInitialTransaction(txB);
+
+
+        // Create session with CounterParty
+        progressTracker.setCurrentStep(COUNTERPARTY_SESSION);
+        FlowSession otherPartySession = initiateFlow(newOwner);
+        FlowSession mintPartySession = initiateFlow(issuer);
+
+
+        // Finalize and send to CounterParty
+        progressTracker.setCurrentStep(FINALISING_TRANSACTION);
+        return subFlow(new FinalityFlow(signedTx, otherPartySession, mintPartySession));
+
+
+    }
+}
